50f69f8b8cb6a1ea318a72a452dd6e109668db82
Gustavo Bordoni <gusbordoni.dev@gmail.com>
2022-08-04 23:53:39 -0300

    api finally working

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
-import {
-  API_KEY, BASE_URL,
-  IMG_URL,
-  language,
-} from './api.js'
+const API_KEY = 'api_key=6deb0d8ac5e8817e9d9d7b6c5127e97b';
+const BASE_URL = 'https://api.themoviedb.org/3/movie/';
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+const language = 'language=pt-BR';
 
 
-function getMovie1(movieID) {
+function getMovie(movieID) {
   axios.get(`${BASE_URL}${movieID}?${API_KEY}`)
-  .then(response => movie1.textContent = "ok")
+  .then(response => movie1.textContent = response.data.original_title)
   .catch(error => console.error(error))
+}
+
+function wtfBro() {
+  console.log('wtffff')
 }
 
 /* 453395 */
