8c521ddf53915fae97445f5bc532da121d8ef3d7
Gustavo do Amaral Bordoni <gustavo.bordoni@relux.local>
2022-08-05 12:16:21 -0300

    v1.0 done

--- main.js
+++ main.js
@@ -4,14 +4,17 @@ const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const language = 'language=pt-BR';
 
 
-function getMovie(movieID) {
-  axios.get(`${BASE_URL}${movieID}?${API_KEY}`)
-  .then(response => movie1.textContent = response.data.original_title)
+function getPopularMovie() {
+  const randomPage = Math.floor(Math.random() * 500);
+  const randomItem = Math.floor(Math.random() * 20);
+  axios.get(`${BASE_URL}popular?${API_KEY}&${language}&page=${randomPage}`)
+  .then(response => {
+    moviePoster.src = IMG_URL+response.data.results[randomItem].poster_path
+    movieTitle.textContent = response.data.results[randomItem].title
+    movieDesc.textContent = response.data.results[randomItem].overview
+  })
   .catch(error => console.error(error))
 }
 
-function wtfBro() {
-  console.log('wtffff')
-}
-
-/* 453395 */
+/* tmdb key v3
+6deb0d8ac5e8817e9d9d7b6c5127e97b */
