d4ec2ba8d1369881cf0fbd8a8ed04868b1cb3369
Gustavo do Amaral Bordoni <gustavo.bordoni@relux.local>
2022-08-29 16:58:48 -0300

    general update

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# WIP rocketflix
+# 💜  "Rocketflix" API Challenge by RocketSeat
+
+### :rocket: Github Pages
+
+- Clique aqui para visualizar o projeto funcionando: [Rocketflix Challenge](https://gusbordoni.github.io/rocketflix/)
+
+### :computer: Tecnologias
+
+<p align="justify">
+
+- HTML
+- CSS
+- JAVASCRIPT
+- Node.js / Axios
+
+</p>
+
+### :page_facing_up: Sobre
+
+<p align="justify">
+
+-  Desafio API proposto pela Rocketseat, após curso "Discover"
+-  Integração de API do site [The Movie Database](https://www.themoviedb.org/) com randomização de resultados
+
+</p>
+
+ desafio [@rocketseat](https://www.rocketseat.com.br/)
--- main.js
+++ main.js
@@ -2,19 +2,66 @@ const API_KEY = 'api_key=6deb0d8ac5e8817e9d9d7b6c5127e97b';
 const BASE_URL = 'https://api.themoviedb.org/3/movie/';
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const language = 'language=pt-BR';
+var movieID;
 
-
-function getPopularMovie() {
-  const randomPage = Math.floor(Math.random() * 500);
+async function getRandomMovie() {
+try {
+  const randomPage = Math.floor(Math.random() * 499 + 1);
   const randomItem = Math.floor(Math.random() * 20);
-  axios.get(`${BASE_URL}popular?${API_KEY}&${language}&page=${randomPage}`)
-  .then(response => {
-    moviePoster.src = IMG_URL+response.data.results[randomItem].poster_path
-    movieTitle.textContent = response.data.results[randomItem].title
-    movieDesc.textContent = response.data.results[randomItem].overview
-  })
-  .catch(error => console.error(error))
+  const genreList = [];
+  const buyList = [];
+  const rentList = [];
+
+  const moviesPage = await axios.get(`${BASE_URL}popular?${API_KEY}&${language}&page=${randomPage}`)
+  const movieID = await moviesPage.data.results[randomItem].id
+  const movieDetails = await axios.get(`${BASE_URL}${movieID}?${API_KEY}&${language}`)
+  const movieAvailability = await axios.get(`${BASE_URL}${movieID}/watch/providers?${API_KEY}`)
+
+  const mLink = 'https://www.themoviedb.org/movie/' + movieDetails.data.id
+
+/*   console.log(movieDetails.data.id)
+  console.log(movieDetails.data) */
+  /* console.log(movieAvailability.data.results.BR.buy)
+  console.log(movieAvailability.data.results.BR.rent) */
+
+  for(i = 0; i < movieAvailability.data.results.BR.flatrate[0].length; i++){
+    buyList.push(movieAvailability.data.results.BR.flatrate[0].provider_name)
+  }
+  console.log(movieAvailability.data.results.BR.flatrate[0].provider_name)
+// aquiiiiiiiiiiiiiiiiiiiiii
+  moviePoster.style.display = 'flex'
+  moviePoster.src = IMG_URL+movieDetails.data.poster_path
+  movieTitle.innerHTML = `<a href="${mLink}" target="_blank" class="titleLink"> ${movieDetails.data.title} (${movieDetails.data.release_date.slice(0,4)})</a>`
+
+  for(i = 0; i < movieDetails.data.genres.length; i++){
+    genreList.push(movieDetails.data.genres[i].name)
+  }
+  movieGenre.textContent = genreList.join(' • ')
+
+  if(movieDetails.data.vote_average != 0) {
+    movieRating.textContent = 'NOTA TMDB: ' + movieDetails.data.vote_average.toFixed(1)  
+  } else {
+    movieRating.textContent = '(Não há votos suficientes para exibir uma nota para o filme.)'
+  }
+
+  if(movieDetails.data.overview != "") {
+    movieDesc.textContent = movieDetails.data.overview  
+  } else {
+    movieDesc.innerHTML = `<p class="noDescText">Não há descrição do filme em português. Saiba mais em: <br/><a href="${mLink}" target="_blank" class="noDescLink">${movieDetails.data.title} - At The Movie Database <img class="noDescImg" src="./assets/new-window.svg"></a></p>`
+  }
+
+  if(movieDetails.data.runtime/60 < 1) {
+    movieRuntime.textContent = 'Duração: ' + (movieDetails.data.runtime % 60) + 'min'
+  } else {
+    movieRuntime.textContent = 'Duração: ' + Math.floor(movieDetails.data.runtime / 60) + 'h' +  (movieDetails.data.runtime % 60) + 'min'
+  }
+
+
+} catch (error) {
+  console.log(error)
 }
+}
+
 
 /* tmdb key v3
 6deb0d8ac5e8817e9d9d7b6c5127e97b */
